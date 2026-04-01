b2e11a9e899ba0b29c19f9d3fa1b13e291b1b446
Yug Solanki <yugsolanki15@gmail.com>
2026-04-01 23:28:51 +0530

    feat: implement basic web server to serve HTML and images

--- main.go
+++ main.go
@@ -1,7 +1,28 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"net/http"
+)
 
 func main() {
 	fmt.Println("Hello World")
+
+	// Basic Web Server serving html and images from /images folder
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		if r.URL.Path != "/" {
+			http.NotFound(w, r)
+			return
+		}
+		http.ServeFile(w, r, "./index.htm")
+	})
+
+	fs := http.FileServer(http.Dir("images"))
+	http.Handle("/images/", http.StripPrefix("/images/", fs))
+
+	fmt.Println("Server is listening on port 8080...")
+	err := http.ListenAndServe(":8080", nil)
+	if err != nil {
+		fmt.Println("Error starting server:", err)
+	}
 }
